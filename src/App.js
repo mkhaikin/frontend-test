77284aeaadf4d0b7bf94b0a1649bca47e4cc4f0d
yefimkh <michael.khaikin@gmail.com>
2021-09-05 21:26:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+
+  const submit = async event =>{
+    event.preventDefault()
+    const data = new FormData()
+    data.append('image', file)
+    data.append('description', description)
+    await axios.post('http://localhost:8080/posts', data).then(response=>{
+      console.log(response)
+    })
+    
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1> App Front Base</h1>
+        <form onSubmit={submit}>
+          <input
+            filename={file} 
+            onChange={e => setFile(e.target.files[0])} 
+            type="file" 
+            accept="image/*"
+          ></input>
+          <input
+            onChange={e => setDescription(e.target.value)} 
+            type="text"
+            placeholder="description"
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
